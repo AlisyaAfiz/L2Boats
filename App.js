b10a1059f7d01f68deab67859cbadae44da0eef0
AlisyaAfiz <23018913@myrp.edu.sg>
2024-11-08 21:50:52 +0800

    Updated Folders

--- App.js
+++ App.js
@@ -1,24 +1,29 @@
 import React from 'react';
-import {View, Text, Image, ScrollView} from 'react-native';
-import Boat from './components/Boat';
+import {View, Text, ScrollView, StyleSheet} from 'react-native';
+import Boat from './components/Boats';
+
+const styles = StyleSheet.create({
+        title: {
+            fontSize: 30,
+            textAlign: 'center',
+            color: 'white',
+            backgroundColor: 'skyblue',
+            fontWeight: 'bold',
+            padding: 20
+        }
+});
 
 const AllBoats =() => {
   return (
       <ScrollView>
-          <Text></Text>
-          <Text></Text>
-          <Text>GetABoat - For Sale</Text>
-          <Text></Text>
+              <View style={{marginTop: 50}}>
+          <Text style={styles.title}>GetABoat - For Sale</Text>
+              </View>
           <Boat icon_name="boat" name="Sea Ray 500 Sundancer" description="Contoured lines and dramatic styling reveal a refined and powerful presence that will take your breath away." picture={require('./img/sea_ray.jpg')}/>
-          <Text></Text>
           <Boat icon_name="boat" name="Four Winns Horizon 180" description="A sporty look and refined details truly set the Horizon 180 above all others." picture={require('./img/four_winns.jpg')}/>
-          <Text></Text>
           <Boat icon_name="boat" name="Flipper 640 ST" description="A modern take on the classic, traditional hardtop and perfect for a family picnic." picture={require('./img/flipper.jpg')}/>
-          <Text></Text>
           <Boat icon_name="boat" name="Princess V48" description="There is the option for an open design with a full-length cockpit and sunroof, or the enclosed deck saloon model, available with the option of a climate controlled interior." picture={require('./img/princess.jpg')}/>
-          <Text></Text>
           <Boat icon_name="boat" name="Bayliner 175 Bowrider" description="Its outboard power gives you increased cockpit space and quiet, fuel-efficient performance." picture={require('./img/bayliner.jpg')}/>
-          <Text></Text>
           <Boat icon_name="boat" name="Fairline Targa 47" description="Stretch out on the large sun bed aft while friends lounge in the generously appointed cockpit." picture={require('./img/fairline.jpg')}/>
       </ScrollView>
   );
--- components/Boats.js
+++ components/Boats.js
@@ -1,16 +1,42 @@
-import {View, Text, Image} from 'react-native';
+import {View, Text, Image, StyleSheet} from 'react-native';
 import Icon from "react-native-vector-icons/Ionicons";
+
+const styles = StyleSheet.create({
+    boats: {
+        flex: 1,
+        flexDirection: 'column',
+        borderColor: 'black',
+        borderWidth: 3,
+        margin: 10,
+        alignItems:'center',
+        padding: 10,
+        backgroundColor: 'lightblue'
+    },
+    desc: {
+        textAlign: 'center',
+        margin: 10,
+        fontStyle: 'italic',
+        backgroundColor: 'skyblue',
+        padding: 7,
+        borderColor: 'black',
+        borderWidth: 2
+    },
+    img: {
+        width:350,
+        height:250
+    }
+});
 
 const Boat = ({name, description, icon_name, picture})=> {
     return (
-        <View>
-            <Text>
-                <Icon name={icon_name} size={30} color="#90D5FF"><Text> </Text>{name}</Icon>
+        <View style={styles.boats}>
+            <Text style={{marginTop: 15, marginBottom: 15}}>
+                <Icon name={icon_name} size={30} color="black"><Text> </Text>{name}</Icon>
             </Text>
-            <Text>
+            <Text style={styles.desc}>
                 {description}
             </Text>
-            <Image source={picture} style={{width:400, height:300}}/>
+            <Image source={picture} style={styles.img}/>
         </View>
     );
 };
